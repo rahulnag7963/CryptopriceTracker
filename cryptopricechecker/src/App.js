755f2931bcfc97644b52f1b4d736aa1ef1bbb3fa
Rahul <rahulnag7963@gmail.com>
2022-01-06 21:58:54 -0800

    stuff

---
+++ cryptopricechecker/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home';
+import Header from './Header';
+import IndividualCoin from './Pages/IndividualCoin';
+function App() {
+
+  return (
+      <Router>
+        <div>
+        <Header />
+          <Routes>
+            <Route path ="/" element = {<Home/>} exact />
+            <Route path = "/IndividualCoin/:CryptoId" element = {<IndividualCoin/>}></Route>
+        </Routes>
+        </div>
+    </Router>    
+  
+  );
+}
+
+export default App;
---
+++ cryptopricechecker/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import GetCryptoInfo from './GetCryptoInfo';
+
+
+ReactDOM.render
+(
+<React.StrictMode>
+    <GetCryptoInfo>
+        <App />
+    </GetCryptoInfo>
+</React.StrictMode>,document.getElementById("root")
+);
+
